a16b5588155516c64dbbc494f65fd2c3359cccb1
guilherme <kadeguilherme@gmail.com>
2020-10-19 09:48:51 -0300

    Iniciando

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect} from 'react';
+import './App.css';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+
+
+import ChatListItem from './components/ChatListItem';
+
+export default () =>{
+
+  const [chatlist,setChatList] = useState([{},{},{}]);
+
+  return (
+    <div className = "app-window"> 
+      
+      <div className = "sidebar">
+          <header>
+              <img className="header-avatar" src ="https://blog.unyleya.edu.br/wp-content/uploads/2017/12/saiba-como-a-educacao-ajuda-voce-a-ser-uma-pessoa-melhor.jpeg" 
+              alt ="">
+              </img>
+
+              <div className="header-buttons">
+                <div className="header-btn">
+                    <DonutLargeIcon style = {{color:'#919191'}}/>
+                </div>
+                <div className="header-btn">
+                <ChatIcon style = {{color:'#919191'}}/>
+                </div>
+                <div className="header-btn">
+                <MoreVertIcon style = {{color:'#919191'}}/>
+                </div>
+              </div>
+          </header>
+
+          <div className="search">
+            <div className="search-input">
+              <SearchIcon fontSize='small' style ={{color:'#919191'}}/>
+              <input type='search' placeholder='Procurar ou começar uma nova conversa'/>
+            </div>
+          </div>
+         
+
+          <div className="chatlist">
+            {chatlist.map((item,key) =>(
+              <ChatListItem
+              key = {key}
+              />
+            ))}
+          </div>
+
+
+
+      </div>
+
+      <div className = "contentarea">
+
+      </div>
+
+    </div>
+
+  )
+}
---
+++ src/components/ChatListItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './ChatListItem.css'
+
+export default () =>{
+
+    return (
+        <div className='chatLisItem'>
+            <img className='chatListItem-avatar' src="https://blog.unyleya.edu.br/wp-content/uploads/2017/12/saiba-como-a-educacao-ajuda-voce-a-ser-uma-pessoa-melhor.jpeg" alt=""/>
+            <div className='chatListItem-lines'>
+                <div className='chatListItem-line'>
+                    <div className='chatListItem-name'>Guilherme</div>
+                    <div className='chatListItem-date'>19:00</div> 
+                </div>
+                <div className='chatListItem-line'>
+                    <div className='chatListItem-lastMsg'>
+                        <p>Opa,tudo bem?</p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    );
+}
+
